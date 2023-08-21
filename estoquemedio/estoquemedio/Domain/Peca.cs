407233710c10991c445fef34afd169048d2891a4
Rubens Santo <r4454406@gmail.com>
2023-08-21 15:40:36 -0300

    Adicionado projeto

---
+++ estoquemedio/estoquemedio/Domain/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Peca
+{
+    // Propriedades da peça
+    public string Nome { get; set; }
+    public int QuantidadeMinima { get; set; }
+    public int QuantidadeMaxima { get; set; }
+
+    // Construtor da classe
+    public Peca(string nome, int quantidadeMinima, int quantidadeMaxima)
+    {
+        Nome = nome;
+        QuantidadeMinima = quantidadeMinima;
+        QuantidadeMaxima = quantidadeMaxima;
+    }
+
+    // Método para calcular o estoque médio
+    public double CalcularEstoqueMedio()
+    {
+        return (QuantidadeMinima + QuantidadeMaxima) / 2.0;
+    }
+}
+
---
+++ estoquemedio/estoquemedio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe os detalhes da peça:");
+        Console.Write("Nome da peça: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Quantidade mínima: ");
+        int quantidadeMinima = int.Parse(Console.ReadLine());
+
+        Console.Write("Quantidade máxima: ");
+        int quantidadeMaxima = int.Parse(Console.ReadLine());
+
+        Peca peca = new Peca(nome, quantidadeMinima, quantidadeMaxima);
+
+        double estoqueMedio = peca.CalcularEstoqueMedio();
+
+        Console.WriteLine($"Estoque médio da peça {peca.Nome}: {estoqueMedio}");
+
+        // Aguarda o usuário pressionar Enter para encerrar o programa
+        Console.ReadLine();
+    }
+}
+
